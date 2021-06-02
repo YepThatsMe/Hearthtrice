0b0e2d510510f284e20d517b8c2611e9cfe29097
YepThatsMe <59745998+YepThatsMe@users.noreply.github.com>
2021-06-02 22:32:33 +0300

    im

--- ImportModule/IM.py
+++ ImportModule/IM.py
@@ -1,2 +1,13 @@
-class IM():
-    pass
+from Hearthtrice.Hearthtrice import MainWindow
+import Hearthtrice
+
+class IM(Hearthtrice.MainWindow, QMainWindow):
+
+
+    def rofl():
+
+        with open(resource_path('assets\how_to_use.txt'), 'r') as b:
+                text = b.read()
+        a = QMessageBox.about(self, 'Help', text)
+
+    
