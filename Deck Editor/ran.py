a8c6718ed3f1a96630b21d70753e55ce994257b4
danildzitiev <danilinclude@yandex.ru>
2021-06-03 19:41:35 +0300

    test class for merge

--- Deck Editor/ran.py
+++ Deck Editor/ran.py
@@ -1,11 +1,5 @@
-from PyQt5.QtWidgets import (QMainWindow, QWidget, QDesktopWidget, QApplication, QAction,
-                             QMessageBox, QFileDialog, QListWidget, QComboBox, QHBoxLayout,
-                             QVBoxLayout, QLabel, QLineEdit, QCompleter, QGridLayout,
-                             QTableWidget, QProgressBar, QPushButton)
+from PyQt5.QtWidgets import QWidget, QPushButton, QHBoxLayout
 
-from PyQt5.QtGui import QIcon, QPixmap
-from PyQt5.QtCore import Qt
-import sys
 class win(QWidget):
     def __init__(self):
         super(win, self).__init__()
